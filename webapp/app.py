8d2cf16ceb2f7189cd3bb460fc32fd89e8a13100
Anand Chitipothu <anandology@gmail.com>
2021-02-09 17:02:18 +0530

    Added skeleton for code editor.

--- webapp/app.py
+++ webapp/app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/sketches/new")
+def new_sketch():
+    sketch = {
+        "title": "New Sketch",
+        "mode": "python",
+        "code": 'print("Hello, world!")'
+    }
+    return render_template("sketches/edit.html", sketch=sketch)
